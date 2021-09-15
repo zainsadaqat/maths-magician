a5c9e95c031efe92f9955b5fdcfc83f09cb97dbd
Ntwali-Josue <kntwalijos@gmail.com>
2021-09-15 13:56:06 +0200

    Import and render the Calculator method

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Calculator from './components/Calculator';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hey</h1>
+      <Calculator />
     </div>
   );
 }
