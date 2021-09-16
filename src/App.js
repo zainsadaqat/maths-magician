e220542c8443628980b96231a5ba7e05a754ea29
Ntwali-Josue <kntwalijos@gmail.com>
2021-09-16 17:05:55 +0200

    Add state with the digit object

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import './App.css';
 import Calculator from './components/Calculator';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      digitObj: {
+        total: null,
+        next: null,
+        operation: null,
+      },
+    };
+  }
+
   render() {
+    const { digitObj } = this.state;
+    console.log(digitObj);
     return (
       <div className="App">
         <Calculator />
