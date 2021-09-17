c34b3dd1f4c6878658e1a9a91601c84b535d4eb1
Ntwali-Josue <kntwalijos@gmail.com>
2021-09-17 11:03:42 +0200

    Remove the import of the proptypes

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/forbid-prop-types */
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import calculate from '../logic/calculate';
 import './Calculator.css';
 
